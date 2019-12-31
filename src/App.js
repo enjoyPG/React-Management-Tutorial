3e33ea4876a7253657661193a236125d36d96e8e
EnjoyPG <lasthere77@gmail.com>
2019-12-31 23:40:41 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -34,30 +34,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Customer
-          id={customers[0].id}
-          image={customers[0].image}
-          name={customers[0].name}
-          birthday={customers[0].birthday}
-          gender={customers[0].gender}
-          job={customers[0].job}
-        />
-        <Customer
-          id={customers[1].id}
-          image={customers[1].image}
-          name={customers[1].name}
-          birthday={customers[1].birthday}
-          gender={customers[1].gender}
-          job={customers[1].job}
-      />
-        <Customer
-          id={customers[2].id}
-          image={customers[2].image}
-          name={customers[2].name}
-          birthday={customers[2].birthday}
-          gender={customers[2].gender}
-          job={customers[2].job}
-        />
+        {customers.map((c) => 
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+        )}
       </div>
     );
   }
