f6f04935031a9858d0850c3dd7bc7c2afdd18071
EnjoyPG <lasthere77@gmail.com>
2019-12-31 23:16:10 +0900

    seperating Component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-}
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '세종대왕',
+    'birthday': '962552',
+    'gender': '남자',
+    'job': '왕'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순녀',
+    'birthday': '861222',
+    'gender': '여자',
+    'job': '주부'
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
+      <div>
+        <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
       />
+        <Customer
+          id={customers[2].id}
+          image={customers[2].image}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+        />
+      </div>
     );
   }
 }
